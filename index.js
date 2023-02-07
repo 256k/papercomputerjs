054342ce4a9e8101ac7c73bf5061a3a8d3f3ff4c
256k <fayek.helmi@gmail.com>
2023-02-06 23:02:16 -0500

    aded ability to add comments to pcasm scripts

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var fs = require('fs');
 const process = require('process');
-process.argv.forEach((val, index) => {
-  console.log(`${index}: ${val}`);
-});
+
 let stack = [0,0,0,0,0];
 
 let lineList = fs.readFileSync(process.argv[2],'utf8', function(err, data) {
@@ -10,10 +8,11 @@ let lineList = fs.readFileSync(process.argv[2],'utf8', function(err, data) {
     else return data;
  });
 
-lineList = lineList.split('\n');
+lineList = lineList.split('\n').filter(line => (line[0] !== '#'));
 
 let splitLineList = lineList.map(line => line.split(' '));
 let i = 0;
+
 while (true) {
   console.log("Step: ", i);
   console.log("Command: ", splitLineList[i][0]);
@@ -25,7 +24,6 @@ while (true) {
   else if (splitLineList[i][0] == "dec") { stack[splitLineList[i][1]-1] -= 1 }
   else if (splitLineList[i][0] == "jmp") {i = splitLineList[i][1] }
   else if (splitLineList[i][0] == "isz") {if (stack[splitLineList[i][1]-1] == 0) {i++}}
-  // else if (splitLineList[i][0] == "isz") {i++}
   else if (splitLineList[i][0] == "stp") {break;}
   i++;
 }
